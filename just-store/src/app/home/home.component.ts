516cc44f168b2766a2e48078fb58433569914d37
Justice <nkopane.mj@gmail.com>
2025-01-31 20:27:50 +0200

    add register form

--- just-store/src/app/home/home.component.ts
+++ just-store/src/app/home/home.component.ts
@@ -4,14 +4,16 @@ import { SearchBarComponent } from "./search-bar/search-bar.component";
 import { ProductsComponent } from "./products/products.component";
 import { NgIf } from '@angular/common';
 import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgIf, NavbarComponent, SearchBarComponent, ProductsComponent, LoginComponent],
+  imports: [NgIf, NavbarComponent, SearchBarComponent, ProductsComponent, LoginComponent, RegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  showLogin: boolean = true;
+  showLogin: boolean = false;
+  showRegister: boolean = true;  
 }
